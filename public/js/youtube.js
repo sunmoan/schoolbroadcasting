8fcdcef06ef3aa1426a95795d79d0bb8b6602ed2
Sunmo An <sunnya0723@gmail.com>
2020-04-12 03:24:17 +0900

    ??

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -11,15 +11,15 @@ var tag = document.createElement('script');
     var ctrlq = document.getElementById("player");
 
     player = new YT.Player('player', {
-      height: '100',
-      width: '100',
+      height: '0',
+      width: '0',
       videoId: ctrlq.dataset.video,
       events: {
         'onReady': onPlayerReady,
         'onStateChange': onPlayerStateChange 
       },
       playerVars: {
-        autoplay: '0',
+        autoplay: ctrlq.dataset.autoplay,
         loop: ctrlq.dataset.loop,
         
       },
@@ -28,45 +28,59 @@ var tag = document.createElement('script');
   } 
 
   function videostart() {
-    alert("ready1");
+    
     player.playVideo();
-    alert("ready2");
+    player.setVolume('50')
     document.getElementById("youtube-audio-play").style.display = "none";
     document.getElementById("youtube-audio-loading").style.display = "inline";
-    document.getElementById("progressbar").style.display = "";
+    document.getElementById("progressbar-loading").style.display = "";
     
+   
   }
 
   function onPlayerReady(event) {
-    alert("readasasy");
-    event.target.playVideo();
-    alert("readasasy");
+ 
+    
+    
   }
 
   function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING) { //재생중이면
+      
+      document.getElementById("progressbar-playing").style.display = "inline";
+      document.getElementById("progressbar-playing").style.display = "inline";
+      document.getElementById("youtube-audio-play").style.display = "none"; //재생하기 삭제
       document.getElementById("youtube-audio-loading").style.display = "none"; //로딩중 삭제
-      document.getElementById("progressbar").style.display = "none"; //로딩바 삭제
+      document.getElementById("progressbar-loading").style.display = "none"; //로딩바 삭제
+      document.getElementById("youtube-audio-fromfirst").style.display = "none"; //처음부터 듣기 삭제
       document.getElementById("youtube-audio-playing").style.display = "inline"; //재생중 표시
       document.getElementById("youtube-audio-pause").style.display = "inline"; //일시정지하기 표시
+      document.getElementById("youtube-audio-paused").style.display = "none"; //일시정지중 삭제
+      document.getElementById("youtube-audio-resume").style.display = "none"; //이어서 듣기 삭제
+
     } else if (event.data == YT.PlayerState.PAUSED) { //일시정지중이면
+      document.getElementById("youtube-audio-fromfirst").style.display = "inline"; //처음부터 듣기 표시
       document.getElementById("youtube-audio-playing").style.display = "none"; //재생중 삭제
       document.getElementById("youtube-audio-pause").style.display = "none"; //일시정지학기 삭제
       document.getElementById("youtube-audio-paused").style.display = "inline"; //일시정지중 표시
       document.getElementById("youtube-audio-resume").style.display = "inline"; //이어서 듣기 표시
-    }
+    } 
   }
 
 
 
   function videopause() {
-
+    player.pauseVideo();
   }
 
   function videoresume() {
-
+    player.playVideo();
   }
   
+  function videofromfirst() {
+    player.seekTo(0,false);
+    player.playVideo();
+  }
 
 
   function toggleAudio() {
